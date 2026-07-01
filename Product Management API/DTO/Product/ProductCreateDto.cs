bba2f73120898e6a608fd54c8a03974555a4df58
Ergi <150471039+ErgiKuka@users.noreply.github.com>
2026-07-01 10:53:08 +0200

    Fixed issues on entities and services

--- Product Management API/DTO/Product/ProductCreateDto.cs
+++ Product Management API/DTO/Product/ProductCreateDto.cs
@@ -5,7 +5,7 @@
         public string ProductName { get; set; }
         public string? Description { get; set; }
         public decimal Price { get; set; }
-        public int StockQuantity { get; set; }
+        public int StockQuantity { get; set; } = 0;
         public int CategoryId { get; set; }
     }
 }
